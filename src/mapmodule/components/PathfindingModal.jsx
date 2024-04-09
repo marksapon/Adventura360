2976277e7e03588b9edb2a5c628bab16de6fc116
Mark Sapon <mackysapon23@gmail.com>
2024-04-09 15:20:11 +0800

    DATE:09/04/2024 Map Module
    [/] Path Display

--- src/mapmodule/components/PathfindingModal.jsx
+++ src/mapmodule/components/PathfindingModal.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
+import { initOSDFabricJS } from "openseadragon-fabric";
 
 /* Components */
-// import PathFindingModalSearch from "./PathfindingModalSearch"; // Pathfinding Modal Search
+import Pathfinding from "./Pathfinding";
 
 /* React Icons */
 import { ImLocation2 } from "react-icons/im"; // Current Location On Button
@@ -10,17 +11,18 @@ import { FaCarAlt } from "react-icons/fa"; // Drive State
 import { MdOutlineGpsFixed } from "react-icons/md"; // Target Location Button
 import { IoIosClose } from "react-icons/io"; // Close Button
 import { MdDirections } from "react-icons/md"; // Directions Button
+import { useEffect } from "react";
 
-const PathfindingModal = ({ ref, setPathModalState }) => {
-  console.log("Pathfinding Modal");
-
-  /* States */
-  const [walkState, setWalkState] = useState(true); // Walk State
+const PathfindingModal = ({ setPathModalState, currLoc, viewer }) => {
+  /* UI States  */
+  const [walkState, setWalkState] = useState(true); // Travel Modes: True = Walk / False = Vehicle
   const [buildingsListState, setBuildingsListState] = useState(false); // Buildings List State
-  const [destination, setDestination] = useState(""); // Destination Holder
-  const [directionState, setDirectionState] = useState(false); // Direction State
+  const [directionState, setDirectionState] = useState(false); // Direction Button State
+  const [destination, setDestination] = useState(""); // Destination Variable
 
-  /* Building Search */
+  /* Buildings Search */
+
+  // Buildings List
   const buildings = [
     "Building 1",
     "Building 2",
@@ -29,9 +31,11 @@ const PathfindingModal = ({ ref, setPathModalState }) => {
     "Building 5",
   ];
 
-  const [buildingsList, setBuildingsList] = useState(buildings);
+  const [buildingsList, setBuildingsList] = useState(buildings); // Pass Buildings List
 
+  // Search Function Event that triggers everytime the search value changes
   const search = (event) => {
+    // If Search Bar is empty Set Buildings List to Default and Hide Directions Button
     if (event.target.value === "") {
       setBuildingsList(buildings);
       setBuildingsListState(false);
@@ -39,8 +43,9 @@ const PathfindingModal = ({ ref, setPathModalState }) => {
       return;
     }
 
-    setDestination(event.target.value);
+    setDestination(event.target.value); // Set Destination to Search Value
 
+    // Filter the building list return the items that match the search value
     const buildingsListFiltered = buildings.filter((building) => {
       setBuildingsListState(true);
       return (
@@ -48,31 +53,69 @@ const PathfindingModal = ({ ref, setPathModalState }) => {
       );
     });
 
+    // Set the filtered list to the buildings list
     setBuildingsList(buildingsListFiltered);
   };
 
+  /* Path Display */
+
+  const [pathFabric, setPathFabric] = useState(); // Stores Fabric Path
+  const [fabricCanvas, setFabricCanvas] = useState(); // Fabric Canvas
+  const [fabricLoaded, setFabricLoaded] = useState(false); // Fabric Loaded State
+
+  // useEffect kicks in when fabric path from Pathfinding component changes
+  useEffect(() => {
+    console.log("useEffect Pathfinding Modal");
+    /* Path FabricJS */
+    initOSDFabricJS(); // Initialize FabricJS
+
+    // Create a Fabric Overlay Instance
+    const fabricOverlay = viewer.fabricOverlay({
+      fabricCanvasOptions: { selection: false },
+    });
+
+    setFabricCanvas(fabricOverlay); // Set Fabric Canvas
+    setFabricLoaded(true); // Set Fabric is Loaded State
+  }, [pathFabric]);
+
   return (
     <div className="z-50 ml-2 mr-2 flex h-full w-auto items-baseline justify-center">
+      {/* Pathfinding Component */}
+      <Pathfinding
+        viewer={viewer}
+        currloc={currLoc}
+        targetLoc={destination}
+        pathFabric={setPathFabric}
+      />
+      {/* Pathfinding Component */}
       <div className="justify-baselign w-30 flex flex-col items-center space-y-1">
         <div className="pointer-events-auto relative z-10 grid h-auto grid-cols-4 grid-rows-3 gap-2 rounded-xl border-2 border-gray-300 bg-slate-50 shadow-lg sm:w-full lg:w-full">
+          {/* Close Button */}
           <button
             className="absolute right-0 z-20 m-1"
             onClick={() => {
               setPathModalState(false);
+              fabricCanvas.fabricCanvas().clear();
             }}
           >
             <IoIosClose size={30} />
           </button>
+          {/* Close Button */}
+          {/* Current Location Icon */}
           <div className=" mt-3 flex items-end justify-end">
             <div className="flex h-full w-10 items-center justify-center text-green-600">
               <ImLocation2 size={25} />
             </div>
           </div>
+          {/* Current Location Icon */}
+          {/* Current location Container */}
           <div className="col-span-2 mt-3 flex items-center justify-center">
             <div className="flex h-full w-full items-center justify-center rounded-md border-2 border-gray-500 bg-gray-400 text-white">
               Current location
             </div>
           </div>
+          {/* Current location Container */}
+          {/* Walk State Button */}
           <div className="pointer-events-auto mt-3 flex items-center">
             <button
               className={`flex h-full w-10 items-center justify-center rounded-2xl border-2 ${walkState ? "bg-blue-500 text-white" : "text-blue-500"} border-blue-500`}
@@ -83,11 +126,15 @@ const PathfindingModal = ({ ref, setPathModalState }) => {
               <FaWalking size={25} />
             </button>
           </div>
+          {/* Walk State Button */}
+          {/* Destination Icon */}
           <div className=" mb-2 flex items-center justify-end">
             <div className="flex h-full w-10 items-center justify-center text-orange-500">
               <MdOutlineGpsFixed size={25} />
             </div>
           </div>
+          {/* Destination Icon */}
+          {/* Destination Container */}
           <div className="col-span-2 mb-2 flex justify-center">
             <input
               className="flex h-full w-full items-center justify-center rounded-md border-2 border-gray-500 bg-white text-center"
@@ -100,6 +147,8 @@ const PathfindingModal = ({ ref, setPathModalState }) => {
               value={destination}
             />
           </div>
+          {/* Destination Container */}
+          {/* Vehicle State Button */}
           <div className="mb-2 flex items-center">
             <button
               className={`flex h-full w-10 items-center justify-center rounded-2xl border-2 ${!walkState ? "bg-blue-500 text-white" : "text-blue-500"} border-blue-500`}
@@ -110,21 +159,27 @@ const PathfindingModal = ({ ref, setPathModalState }) => {
               <FaCarAlt size={25} />
             </button>
           </div>
+          {/* Vehicle State Button */}
+          {/* Directions Button */}
           <div className="col-span-2 col-start-2 flex items-baseline justify-center ">
-            {directionState ? (
+            {directionState && (
               <button
                 className=" relative flex h-8 w-32 items-center justify-center rounded-md bg-green-500 text-white"
                 onClick={() => {
-                  console.log("Show Path");
+                  if (fabricLoaded) {
+                    fabricCanvas.fabricCanvas().add(pathFabric);
+                  }
                 }}
               >
                 Directions
                 <MdDirections className="absolute right-0 p-0.5" size={25} />
               </button>
-            ) : null}
+            )}
           </div>
+          {/* Directions Button */}
         </div>
-        {buildingsListState ? (
+        {/* Buildings List Container */}
+        {buildingsListState && (
           <div className="justify-baseline pointer-events-auto flex w-full flex-col space-y-1 overflow-scroll rounded-lg border-2 border-gray-300 bg-white p-2 shadow-lg sm:h-20 lg:h-1/2">
             {buildingsList.map((building, index) => (
               <button
@@ -140,7 +195,8 @@ const PathfindingModal = ({ ref, setPathModalState }) => {
               </button>
             ))}
           </div>
-        ) : null}
+        )}
+        {/* Buildings List Container */}
       </div>
     </div>
   );
