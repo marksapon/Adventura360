3936caf34c5fc37fa3b481db56a659caa0cc7051
Kevin Roi Nuesca <kevinroi.nuesca@cvsu.edu.ph>
2024-05-22 23:10:13 +0800

    latest nodes

--- src/module360/components/Sharelink.jsx
+++ src/module360/components/Sharelink.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { IoIosClose } from "react-icons/io";
 import { MdContentCopy } from "react-icons/md";
 
@@ -6,16 +6,15 @@ const Sharelink = ({ visible, onClose }) => {
   if (!visible) return null;
 
   const currentUrl = window.location.href;
+  const [isCopied, setIsCopied] = useState(false);
 
   const copyToClipboard = () => {
     navigator.clipboard
       .writeText(currentUrl)
-      .then(() => {
-        alert("URL copied to clipboard: " + currentUrl);
-      })
       .catch((error) =>
         console.error("Error copying URL to clipboard: ", error),
       );
+    setIsCopied(true);
   };
 
   return (
@@ -36,27 +35,37 @@ const Sharelink = ({ visible, onClose }) => {
                 <IoIosClose className="h-6 w-6 text-black md:h-8 md:w-8 lg:h-10 lg:w-10" />
               </button>
             </div>
-            <div className="mt-4 flex h-full w-full items-center justify-center">
+            <div className="mt-4 flex h-full w-full flex-col items-center justify-between">
               <p className="text-2xl font-bold text-green-600">
                 Share what you see!
               </p>
             </div>
           </div>
         </div>
-        <div className="my-2 flex h-full w-full items-center justify-between gap-4 px-2">
-          <div className="flex h-full items-center justify-center rounded-lg bg-gray-200 p-3 text-green-600 ">
-            <button
-              onClick={() => {
-                copyToClipboard();
-              }}
-            >
-              <MdContentCopy className="h-6 w-6" />
-            </button>
-          </div>
-          <div className="flex h-full w-full items-center rounded-lg bg-gray-200 p-4">
-            <p className="sm:2xl md:2xl lg:3xl text-sm text-gray-700">
-              {currentUrl}
-            </p>
+
+        <div className="my-2 flex h-full w-full flex-col items-center justify-start px-2">
+          <div className="flex gap-4">
+            <div className="flex h-full items-start justify-start rounded-lg bg-gray-200 p-3 text-green-600">
+              <button
+                onClick={() => {
+                  copyToClipboard();
+                }}
+              >
+                <MdContentCopy className="h-6 w-6" />
+              </button>
+            </div>
+            <div className="flex flex-col gap-1">
+              <div className="flex h-full w-full items-center rounded-lg bg-gray-200 p-4">
+                <p className="sm:2xl md:2xl lg:3xl text-sm text-gray-700">
+                  {currentUrl}
+                </p>
+              </div>
+              {isCopied && (
+                <p className="flex w-full items-center justify-start text-center text-xs text-green-600">
+                  Link copied!
+                </p>
+              )}
+            </div>
           </div>
         </div>
       </div>
