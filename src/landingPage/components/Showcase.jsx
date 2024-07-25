3b26048765b03c4163f45c425e9aaccc2b22f44b
Kevin Roi Nuesca <kevinroi.nuesca@cvsu.edu.ph>
2024-07-25 16:01:08 +0800

    latest

--- src/landingPage/components/Showcase.jsx
+++ src/landingPage/components/Showcase.jsx
@@ -20,59 +20,64 @@ const Showcase = () => {
               className="object-fill"
             />
           </div>
-          <h1 className="col-span-1 text-center md:col-span-4">
-            <b className="text-green-700">Cavite State University</b>, is simply
-            dummy text of the printing and typesetting industry. Lorem Ipsum has
-            been the industry's standard dummy text ever since the 1500s, when
-            an unknown printer took a galley of type and scrambled it to make a
-            type.
-          </h1>
+          <div className="col-span-1 flex items-center justify-center md:col-span-4">
+            <h1 className="text-center">
+              <span className="font-bold text-green-700">
+                Cavite State University
+              </span>
+              , is simply dummy text of the printing and typesetting industry.
+              Lorem Ipsum has been the industry's standard dummy text ever since
+              the 1500s, when an unknown printer took a galley of type and
+              scrambled it to make a type.
+            </h1>
+          </div>
         </div>
       </div>
 
       <div className="my-8 flex w-3/4">
         <div className="flex w-full flex-col items-center justify-center gap-4 rounded-3xl  bg-orange-100 p-8 shadow-md shadow-black/30">
-          <h1 className="my-8 text-3xl font-medium">FEATURES</h1>
-          <h2 className="mb-16 text-center text-7xl font-semibold leading-snug text-green-700">
+          <h1 className="my-8 text-2xl font-medium md:text-3xl">FEATURES</h1>
+          <h2 className="mb-16 text-center text-5xl font-semibold leading-snug text-green-700 md:text-7xl">
             Experience a <br />
             feature-rich interactive tour
           </h2>
+
           <div className="flex flex-col gap-4">
-            <div className="flex justify-between gap-4 ">
-              <div className="flex w-3/5 flex-col items-start justify-center gap-8">
-                <h1 className="w-fit rounded-full border border-black px-24 py-2 text-4xl font-bold">
+            <div className="flex flex-col justify-between gap-8 md:flex-row ">
+              <div className="items:start flex flex-col items-center justify-center gap-4 text-center md:items-start md:justify-center md:text-start">
+                <h1 className="w-fit rounded-full border border-black px-24 py-2 text-2xl font-bold lg:text-3xl">
                   360°
                 </h1>
-                <h2 className="text-5xl leading-snug text-green-700">
+                <h2 className="text-3xl leading-snug text-green-700 lg:text-4xl">
                   Accurate depiction of the actual location
                 </h2>
-                <h3 className="text-4xl leading-snug">
+                <h3 className="text-2xl leading-snug lg:text-3xl">
                   Explore the campus through accurate <b>360° images</b> with
-                  built-in support for <b>gyro</b> and <b>VR</b> , available
-                  only on <b>supported mobile devices.</b>
+                  built-in support for <b>gyro</b> and <b>VR</b>, available only
+                  on <b>supported mobile devices.</b>
                 </h3>
               </div>
               <img
                 src="/assets/Landing Page/img1.png"
-                className=" object-contain"
+                className="aspect-square object-contain lg:aspect-auto"
                 alt="img1"
               />
             </div>
 
-            <div className="flex justify-between gap-4 ">
+            <div className="flex flex-col-reverse justify-between gap-8 md:flex-row ">
               <img
                 src="/assets/Landing Page/img1.png"
-                className=" object-contain"
+                className="aspect-square object-contain lg:aspect-auto"
                 alt="img2"
               />
-              <div className="flex w-3/5 flex-col items-start justify-center gap-8">
-                <h1 className="w-fit rounded-full border border-black px-24 py-2 text-4xl font-bold">
+              <div className="items:start flex flex-col items-center justify-center gap-4 text-center md:items-start md:justify-center md:text-start">
+                <h1 className="w-fit rounded-full border border-black px-24 py-2 text-2xl font-bold lg:text-3xl">
                   Map
                 </h1>
-                <h2 className="text-5xl leading-snug text-green-700">
+                <h2 className="text-3xl leading-snug text-green-700 lg:text-4xl">
                   With interactive campus map to find what you’re looking for.
                 </h2>
-                <h3 className="text-4xl leading-snug">
+                <h3 className="text-2xl leading-snug lg:text-3xl">
                   Equipped with an interactive campus map, which gives an
                   overview of the campus, it contains features to find and get
                   directions to the buildings you want to visit.
@@ -80,40 +85,40 @@ const Showcase = () => {
               </div>
             </div>
 
-            <div className="flex justify-between gap-4 ">
-              <div className="flex w-3/5 flex-col items-start justify-center gap-8">
-                <h1 className="w-fit rounded-full border border-black px-24 py-2 text-4xl font-bold">
+            <div className="flex flex-col justify-between gap-8 md:flex-row ">
+              <div className="items:start flex flex-col items-center justify-center gap-4 text-center md:items-start md:justify-center md:text-start">
+                <h1 className="w-fit rounded-full border border-black px-24 py-2 text-2xl font-bold lg:text-3xl">
                   Building Info
                 </h1>
-                <h2 className="text-5xl leading-snug text-green-700">
+                <h2 className="text-3xl leading-snug text-green-700 lg:text-4xl">
                   Check out every buildings in the campus
                 </h2>
-                <h3 className="text-4xl leading-snug">
+                <h3 className="text-2xl leading-snug lg:text-3xl">
                   Get informed about the building’s services, information and
                   courses offered by the campus.
                 </h3>
               </div>
               <img
                 src="/assets/Landing Page/img1.png"
-                className=" object-contain"
+                className="aspect-square object-contain lg:aspect-auto"
                 alt="img3"
               />
             </div>
 
-            <div className="flex justify-between gap-4 pb-16">
+            <div className="flex flex-col-reverse justify-between gap-8 md:flex-row ">
               <img
                 src="/assets/Landing Page/img1.png"
-                className=" object-contain"
+                className="aspect-square object-contain lg:aspect-auto"
                 alt="img2"
               />
-              <div className="flex w-3/5 flex-col items-start justify-center gap-8">
-                <h1 className="w-fit rounded-full border border-black px-24 py-2 text-4xl font-bold">
+              <div className="items:start flex flex-col items-center justify-center gap-4 text-center md:items-start md:justify-center md:text-start">
+                <h1 className="w-fit rounded-full border border-black px-24 py-2 text-2xl font-bold lg:text-3xl">
                   Tour Guide
                 </h1>
-                <h2 className="text-5xl leading-snug text-green-700">
+                <h2 className="text-3xl leading-snug text-green-700 lg:text-4xl">
                   With a virtual tour guide to accompany your journey
                 </h2>
-                <h3 className="text-4xl leading-snug">
+                <h3 className="text-2xl leading-snug lg:text-3xl">
                   A virtual tour guide that will accompany you through out the
                   experience.
                 </h3>
