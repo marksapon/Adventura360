e3cdd41e90697a52bb8cb3936ed071eca5adcbcc
Mark Sapon <mackysapon23@gmail.com>
2024-08-16 01:32:23 +0800

    - Added vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import AppRouter from "./AppRouter";
+import { Analytics } from "@vercel/analytics/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    //<React.StrictMode>
+  //   <React.StrictMode>
+  <>
     <AppRouter />
-    //</React.StrictMode>
+    <Analytics />
+  </>,
+
+  //   </React.StrictMode>,
 );
